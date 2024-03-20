801cb31368e9ec7495051055f94e01023a22a717
Kawan02 <kawanng39@gmail.com>
2024-03-20 20:57:08 -0300

    Listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
@@ -16,14 +18,27 @@ function Home() {
         },
       });
 
-      console.log(response);
+      //   Vai comecar no 0 e termina no 10
+      //   console.log(response.data.results.slice(0, 10));
+
+      setFilmes(response.data.results.slice(0, 10));
     }
     loadFilmes();
   }, []);
 
   return (
-    <div>
-      <h1>Bem vindo a home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
