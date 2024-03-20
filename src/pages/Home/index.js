e5505dacf5b62d1c2ee57f1acf8ffe8cc82db378
Kawan02 <kawanng39@gmail.com>
2024-03-20 20:36:06 -0300

    Criando nossa home e fazendo um get com axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
+import api from "../../services/api";
+
 function Home() {
-    return (
-        <div>
-            <h1>Bem vindo a home</h1>
-        </div>
-    );
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const apiUrl = "movie/now_playing";
+      // movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
+      const response = await api.get(apiUrl, {
+        params: {
+          api_key: "28fc232cc001c31e8a031f419d0a14ca",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+
+      console.log(response);
+    }
+    loadFilmes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Bem vindo a home</h1>
+    </div>
+  );
 }
 
-export default Home; 
+export default Home;
