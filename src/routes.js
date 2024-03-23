69ad619207887f7a2512254a087fc1a0a69de53f
Kawan02 <kawanng39@gmail.com>
2024-03-23 13:09:51 -0300

    Criando página de favoritos

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Filme from "./pages/Filme";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Erro from "./pages/Erro";
+import Favoritos from "./pages/Favoritos";
 
 function RoutesApp() {
   return (
@@ -11,6 +12,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
 
         <Route path="*" element={<Erro />} />
       </Routes>
